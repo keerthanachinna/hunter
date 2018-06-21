2c4212d4d47b304869a49091153a78fb090b28eb
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-06-21 23:28:31 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,8 @@
+def main(a, b):
+	ab=str(a)+str(b)
+	ba=str(b)+str(a)
+	return cmp(int(ba), int(ab))
+a = [5,6,7,8,9]
+sorted_array=sorted(a, cmp=comparator)
+num= "".join([str(i) for i in sorted_array])
+print(num)
